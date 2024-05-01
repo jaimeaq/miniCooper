c00a652a38321fafabae9cf01d18167185f48b55
jaimeaq <123690985+jaimeaq@users.noreply.github.com>
2024-05-01 12:42:31 -0400

    Initial setup and configuration of Mini Cooper Spring Backend
    This commit includes the initial setup and configuration for the Mini Cooper Spring Backend. It covers the creation of necessary files such as the project's pom.xml, .gitignore, Maven wrappers and the main application file. Furthermore, it defines application properties and sets up an initial test suite.

---
+++ backend/miniCooper-spring-backend/src/main/java/com/jaq/miniCooperspringbackend/MiniCooperSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jaq.miniCooperspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCooperSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCooperSpringBackendApplication.class, args);
+	}
+
+}
